922171a28454842883c2a18e24bc56ae2edfc13d
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-02-01 21:27:07 +0530

    Trending Movies card list added

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/utils/text.dart';
+import 'package:movies_app/widgets/trending.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
-void main()=> runApp(new myApp());
+void main() => runApp(new myApp());
 
 class myApp extends StatelessWidget {
-  const myApp({ Key? key }) : super(key: key);
+  const myApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Home(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark,
-      primaryColor: Colors.green),
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.green),
     );
   }
 }
 
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  List trendingMovies = [];
+  List topRatedMovies = [];
+  List tv = [];
+  final String API_KEY = "f8f009c0e0f6313e82cc451fb040e452";
+  final readAccessToken =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGYwMDljMGUwZjYzMTNlODJjYzQ1MWZiMDQwZTQ1MiIsInN1YiI6IjYxZWUxMDViOWU0NTg2MDBkOTE5Y2U2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K1T8fS_L171_fULbLNbkG75pDeaxB7keWkIZfNK7oqM";
+
+  @override
+  void initState() {
+    loadMovies();
+    super.initState();
+  }
+
+  loadMovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(ApiKeys(API_KEY, readAccessToken),
+        logConfig: ConfigLogger(showLogs: true, showErrorLogs: true));
+
+    Map trendingResult = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map topRatedResult = await tmdbWithCustomLogs.v3.movies.getTopRated();
+    Map tvResult = await tmdbWithCustomLogs.v3.tv.getPopular();
+    print(trendingResult);
+
+    setState(() {
+      trendingMovies = trendingResult["results"];
+      topRatedMovies = topRatedResult["results"];
+      tv = tvResult["results"];
+    });
+    print(trendingMovies);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("Movie App"),
+        backgroundColor: Colors.transparent,
+        title: modified_text(text: "Movies App", color: Colors.white, size: 24),
         centerTitle: true,
-        backgroundColor: Colors.green,
+      ),
+      body: ListView(
+        children: [
+          TrendingMovies(trending: trendingMovies),
+        ],
       ),
     );
   }
